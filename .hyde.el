028c0e639d28220ba1eae46f727d8bbc9a2b807a
Dave Pearson <davep@davep.org>
2016-05-26 12:49:12 +0100

    Add config file for hyde.

---
+++ .hyde.el
@@ -0,0 +1,9 @@
+(setq
+ hyde/git/remote "origin"
+ hyde/git/remote "master"
+ hyde/deploy-command  ""
+ hyde-custom-params
+ '(
+   ("categories" "[]")
+   ("tags" "[]")
+   ("date" (format-time-string "%Y-%m-%d %H:%M:%S%z"))))
